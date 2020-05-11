c6bc718579a5d24ebb64ce9e3d530c2f36f3b142
SBellido <sebastianbellidodg@gmail.com>
2020-05-11 16:42:12 -0300

    saturation version 2 mas sencilla

--- entregable/js/tp1_entregable.js
+++ entregable/js/tp1_entregable.js
@@ -41,70 +41,98 @@ buttonSave.addEventListener('click', (e) => {
 })
 
 function saturation() {
-    let valueSat = 10
+    let sat = 50
+    let colorSat
+    let colorSaturate = sat + colorSat
     let imageData = ctx.getImageData(0, 0, width, height, alpha) 
     for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        let r = getRed(imageData, x, y)
-        let g = getGreen(imageData, x, y)
-        let b = getBlue(imageData, x, y)
-        let hsl = rgbToHsl(r, g, b)
-        hsl[1] = valueSat
-        let rgb = hslToRgb(hsl[0], hsl[1], hsl[2])
-        setPixel(imageData, x, y, rgb[0], rgb[1], rgb[2], 255)
-      }
+        for (let y = 0; y < height; y++) { 
+            let r = getRed(imageData, x, y)
+            let g = getGreen(imageData, x, y)
+            let b = getBlue(imageData, x, y)
+            if(r > g && r > b) {
+                colorSat = r
+                let colorSaturate = sat + colorSat
+                setPixel(imageData, x, y, colorSaturate, g, b, alpha)
+            } else if(g > r && g > b) {
+                colorSat = g 
+                let colorSaturate = sat + colorSat
+                setPixel(imageData, x, y, r,colorSaturate, b, alpha)
+            } else if(b > r && b > g) {
+                colorSat = b
+                let colorSaturate = sat + colorSat
+                setPixel(imageData, x, y, r, g, colorSaturate, alpha)
+            } 
+        }
     }
     ctx.putImageData(imageData, 0, 0)
 }
 
-function rgbToHsl(r, g, b) {
-    r /= 255, g /= 255, b /= 255
-    let max = Math.max(r, g, b)
-    let min = Math.min(r, g, b)
-    let h, s, l = (max + min) / 2
-    if(max == min) {
-        h = s = 0; // achromatic
-    } else {
-        let d = max - min
-        if(s = l > 0.5) {
-            d / (2 - max - min)
-        } else {
-            d / (max + min)
-        } 
-        switch(max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0)
-            break
-            case g: h = (b - r) / d + 2
-            break
-            case b: h = (r - g) / d + 4
-            break
-        }
-        h /= 6
-    }
-    return [h, s, l]
-}
+// function saturation() {
+//     let valueSat = 10
+//     let imageData = ctx.getImageData(0, 0, width, height, alpha) 
+//     for (let x = 0; x < width; x++) {
+//       for (let y = 0; y < height; y++) {
+//         let r = getRed(imageData, x, y)
+//         let g = getGreen(imageData, x, y)
+//         let b = getBlue(imageData, x, y)
+//         let hsl = rgbToHsl(r, g, b)
+//         hsl[1] = valueSat
+//         let rgb = hslToRgb(hsl[0], hsl[1], hsl[2])
+//         setPixel(imageData, x, y, rgb[0], rgb[1], rgb[2], 255)
+//       }
+//     }
+//     ctx.putImageData(imageData, 0, 0)
+// }
 
-function hslToRgb(h, s, l) {
-    let r, g, b
-    if (s == 0) {
-      r = g = b = l
-    } else {
-      function hue2rgb(p, q, t) {
-        if (t < 0) t += 1
-        if (t > 1) t -= 1
-        if (t < 1 / 6) return p + (q - p) * 6 * t
-        if (t < 1 / 2) return q
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-      } 
-      let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      let p = 2 * l - q
-      r = hue2rgb(p, q, h + 1 / 3)
-      g = hue2rgb(p, q, h)
-      b = hue2rgb(p, q, h - 1 / 3)
-    }
-    return [r * 255, g * 255, b * 255]
-}
+// function rgbToHsl(r, g, b) {
+//     r /= 255, g /= 255, b /= 255
+//     let max = Math.max(r, g, b)
+//     let min = Math.min(r, g, b)
+//     let h, s, l = (max + min) / 2
+//     if(max == min) {
+//         h = s = 0; // achromatic
+//     } else {
+//         let d = max - min
+//         if(s = l > 0.5) {
+//             d / (2 - max - min)
+//         } else {
+//             d / (max + min)
+//         } 
+//         switch(max) {
+//             case r: h = (g - b) / d + (g < b ? 6 : 0)
+//             break
+//             case g: h = (b - r) / d + 2
+//             break
+//             case b: h = (r - g) / d + 4
+//             break
+//         }
+//         h /= 6
+//     }
+//     return [h, s, l]
+// }
+
+// function hslToRgb(h, s, l) {
+//     let r, g, b
+//     if (s == 0) {
+//       r = g = b = l
+//     } else {
+//       function hue2rgb(p, q, t) {
+//         if (t < 0) t += 1
+//         if (t > 1) t -= 1
+//         if (t < 1 / 6) return p + (q - p) * 6 * t
+//         if (t < 1 / 2) return q
+//         if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+//         return p;
+//       } 
+//       let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+//       let p = 2 * l - q
+//       r = hue2rgb(p, q, h + 1 / 3)
+//       g = hue2rgb(p, q, h)
+//       b = hue2rgb(p, q, h - 1 / 3)
+//     }
+//     return [r * 255, g * 255, b * 255]
+// }
 
 function blurGaussian() {  
     let imageData = ctx.getImageData(0, 0, width, height, alpha) 
